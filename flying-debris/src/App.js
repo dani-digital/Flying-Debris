4e1f534fc0cacdcb656a17e8f4b6408a54e3e55e
Joe Dickinson <Joedog13ish@hotmail.com>
2021-10-19 21:10:51 -0500

    Start

--- flying-debris/src/App.js
+++ flying-debris/src/App.js
@@ -1,25 +1,53 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import './style.css';
+//import { Carousel } from 'react-carousel-minimal';
+//import { About } from './AboutComponent';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+  return(
+    <div id="body">
+      <Header/>
+      <Card/>
+      <ContactContainer/>
     </div>
   );
 }
+
+const Header=()=>{
+  return (
+    <div className='header'>
+      <span className='header-title'>
+            Flying Debris!
+      </span>
+        <br/>
+        <span className="header-text">
+            Just flippin' funny!
+        </span>
+    </div>
+  );
+}
+
+
+const Card = (props) =>{
+  return (
+    <div className={props.className}>
+      <div className="small-div">
+        <i className={props.className}></i>
+        <img src={props.img} alt=''/>
+      </div>
+      <div className="big-div">
+        <span className="div-title">
+            {props.title}
+        </span>
+        <br/>
+        <span>
+          {props.description}
+        </span>
+      </div>
+    </div>
+  );
+}
+
+
 
 export default App;
