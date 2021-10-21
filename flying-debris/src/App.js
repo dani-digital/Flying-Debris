38a3d624ddfd18fa5da0773441549ff78a3d2566
Danielle O'Neal <41247530+droneal11015@users.noreply.github.com>
2021-10-20 22:41:32 -0400

    code cleanup

--- flying-debris/src/App.js
+++ flying-debris/src/App.js
@@ -2,15 +2,14 @@ import React from 'react';
 import './style.css';
 import CarouselImages from './components/CarouselComponent';
 import  About  from './components/AboutComponent';
-import Contact from './components/ContactComponent';
+
 
 function App() {
   return(
     <div id="body">
       <CarouselImages/>
       <About/>
-      <Contact/>
-      {/* {<ContactContainer/> */} 
+      
     </div>
   );
 }
